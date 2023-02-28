07f88de5b8bbdedaa6ab99cfa7eea4090fe04f13
valeriaparra123 <101677456+valeriaparra123@users.noreply.github.com>
2023-02-28 16:33:00 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 video = "";
+objects = "";
 
 function preload()
 {
@@ -14,6 +15,22 @@ function setup() {
 function draw()
 {
    image(video, 0, 0, 480, 380); 
+   if(status != "")
+   {
+    objectDetector.detect(video, gotResult);
+    for (i = 0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Estado: objetos detectados)";
+        document.getElementById("number_of_objects").innerHTML = "Numero de objetos detectados = " + objects.length;
+
+        fill("#FF0000");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x +15, objects[i].y + 15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+    }
+   }
 }
 
 function start()
@@ -28,4 +45,13 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
